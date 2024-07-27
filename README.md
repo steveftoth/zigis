c58e39b15094a23d601b71b19af56de2211746c6
Steven Toth <steve@toths.info>
2024-07-26 21:43:33 -0700

    checkpoint, added simple parsing

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## zigis - a Redis server written in zig from scratch
+
+
+## Project Goals
+
+* Provide a basic implementation of the redis key value store in zig.
+* Implement the service so that it can be reused from other processes, the main goal being as a library for use in a postgres plugin
+* Clean room implementation based on protocol only. No copying code or porting code from redis C version.
+* Learn and have fun!
--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,35 @@ const std = @import("std");
 
 const print = std.debug.print;
 
+const ParseError = error{
+    Unimplemented,
+};
+
 const RedisCommands = enum {
     AUTH,
     ECHO,
     GET,
     SET,
     QUIT, // for debugging
-
+    pub fn toString(self: RedisCommands) []const u8 {
+        return switch (self) {
+            RedisCommands.AUTH => "AUTH",
+            RedisCommands.ECHO => "ECHO",
+            RedisCommands.GET => "GET",
+            RedisCommands.SET => "SET",
+            RedisCommands.QUIT => "QUIT",
+        };
+    }
 };
 
-const RedisParsedCommand = struct { command: RedisCommands };
+const RedisParsedCommand = struct {
+    command: RedisCommands = RedisCommands.QUIT,
+    args: [][]u8,
+};
+
+const ServerConfig = struct {
+    port: u16,
+};
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -32,7 +51,11 @@ pub fn main() !void {
         defer line.clearRetainingCapacity();
 
         print("--{s}\n", .{line.items});
-        //var command =  readLine(stdin);
+        if (parseCommand(line.items)) |command| {
+            print("parsed command of type {s}", .{command.command.toString()});
+        } else |_| {
+            print("GotParseError", .{});
+        }
     } else |err| switch (err) {
         else => return err,
     }
@@ -40,7 +63,23 @@ pub fn main() !void {
 
 fn readLine() !void {}
 
-fn parseCommand() !void {}
+fn parseCommand(line: []const u8) !RedisParsedCommand {
+    var splits = std.mem.split(u8, line, " ");
+    var parsedCommand = RedisParsedCommand{
+        .command = RedisCommands.GET,
+        .args = &[0][]u8{},
+    };
+
+    if (splits.next()) |scommand| {
+        const command = scommand;
+        if (std.mem.eql(u8, command, "get")) {
+            parsedCommand.command = RedisCommands.GET;
+        } else {
+            return ParseError.Unimplemented;
+        }
+    }
+    return parsedCommand;
+}
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
